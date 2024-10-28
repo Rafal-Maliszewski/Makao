9f94ac6eb05a93869f448d39e20a152c3dc159e4
Rafal-Maliszewski <rafal.p.maliszewski@gmail.com>
2024-10-28 20:57:44 +0100

    Update Makao 2.py
    - input errors handled
    - deck refilling added

--- Makao 2.py
+++ Makao 2.py
@@ -14,10 +14,17 @@ class talia: #talia w grze
     def wyswietl(self):
         print (self.karty)
 
+    def policz(self):
+        #print (len(self.karty))
+        return(len(self.karty))
+
     def losowanie(self): #losuje, zwraca i usuwa kartę z talii
         karta = random.choice(self.karty)
         self.karty.remove(karta)
         return karta
+    
+    def usun(self,karta):
+        self.karty.remove(karta)
 
 class reka:#karty na rece gracza
     def __init__(self, karty):
@@ -26,17 +33,23 @@ class reka:#karty na rece gracza
     def dobierz(self, nowa_karta):
         self.karty.append(nowa_karta)
 
-    def wybor(self,karta=1000):
-        print_delay ("Twoje karty: "+ str(self.karty),0.005)
-        #karta=1000           
+    def wybor(self,karta=1000):           
         while karta not in range(len(self.karty)):
+            print_delay ("Twoje karty: "+ str(self.karty),0.005)
             karta = input("Wybierz kartę (1-" + str(len(self.karty))+") lub dobierz (0): ")
-            karta = int(karta)
-            if karta==0:
-                print_delay("dobierasz kartę")
-                return 0
-            karta -=1
-            print_delay('wybrales: '+str(self.karty[int(karta)]))
+            try:
+                karta = int(karta)
+                #print ("id "+str(karta))
+                if karta==0:
+                    print_delay("dobierasz kartę")
+                    return 0
+                karta -=1
+                try:
+                    print_delay('wybrales: '+str(self.karty[int(karta)]))
+                except IndexError:
+                    print_delay("\nNie masz tylu kart")
+            except ValueError:
+                print_delay("\nWpisz cyfrę")
         return self.karty[karta]
 
     def usun(self,usun):
@@ -92,30 +105,42 @@ stos_kart.dodanie(talia_uzyta.losowanie())
 #talia_uzyta.wyswietl()
 
     #ROZGRYWKA WŁAŚCIWA
+def rzucanie_karty(rzucona_karta):
+    if rzucona_karta!=0:
+        x,y = stos_kart.karty[-1].split(' ')
+        a,b = [' ',' ']
+        if x != a and y != b: #SPRAWDZENIE CZY KARTA PASUJE DO TEJ NA STOSIE
+            a,b = rzucona_karta.split(' ')
+            if x != a and y != b:
+                print_delay("\nNie możesz rzucić tej karty!")
+                rzucanie_karty(rzucona_karta = gracz1.wybor())
+            else:
+                gracz1.usun(rzucona_karta)
+                #gracz1.wyswietl()
+                stos_kart.dodanie(rzucona_karta)
+                #stos_kart.wyswietl()
+    else:
+        print_delay("dobieraniekarty.exe")
+        gracz1.dobierz(talia_uzyta.losowanie())
+
+def tura_PC(rzucona_karta):
+    pass
+
+
 tura=0
 while len(gracz1.karty) > 0 or len(gracz2.karty) > 0: #WARUNKI ZAKOŃCZENIA GRY
     tura+=1 
+    print(talia_uzyta.policz())
+    if talia_uzyta.policz()==0:
+        print (stos_kart.karty)
+        talia_uzyta = talia(stos_kart.karty)
+        stos_kart=stos([talia_uzyta.karty[-1]])
+        talia_uzyta.usun(talia_uzyta.karty[-1])
+        print (talia_uzyta.karty)
+    
     print_delay ('\ngramy dalej')
     print_delay ("Karta na stole to: " + stos_kart.karty[-1])
-    
-    def rzucanie_karty(rzucona_karta = gracz1.wybor()):
-        if rzucona_karta!=0:
-            x,y = stos_kart.karty[-1].split(' ')
-            a,b = [' ',' ']
-            if x != a and y != b: #SPRAWDZENIE CZY KARTA PASUJE DO TEJ NA STOSIE
-                a,b = rzucona_karta.split(' ')
-                if x != a and y != b:
-                    print_delay("Nie możesz rzucić tej karty!")
-                    rzucanie_karty(rzucona_karta = gracz1.wybor())
-                else:
-                    gracz1.usun(rzucona_karta)
-                    #gracz1.wyswietl()
-                    stos_kart.dodanie(rzucona_karta)
-                    #stos_kart.wyswietl()
-        else:
-            print_delay("dobieraniekarty.exe")
-            gracz1.dobierz(talia_uzyta.losowanie())
-    rzucanie_karty()
+    rzucanie_karty(gracz1.wybor())
 
 print_delay ("\n------WYYGRAŁEŚ!------\nSkonczyłeś grę w "+str(tura)+" turach\n------GRATULACJE------",0.1)
 
