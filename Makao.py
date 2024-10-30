c454f73aa2282b9df0e50f360496019d52bc1169
Rafal-Maliszewski <rafal.p.maliszewski@gmail.com>
2024-10-30 10:19:31 +0100

    Version 1.0
    - menu added
    - code cleaned

--- Makao.py
+++ Makao.py
@@ -15,7 +15,6 @@ class talia: #talia w grze
         print (self.karty)
 
     def policz(self):
-        #print (len(self.karty))
         return(len(self.karty))
 
     def losowanie(self): #losuje, zwraca i usuwa kartę z talii
@@ -54,19 +53,15 @@ class reka:#karty na rece gracza
     
     def auto_wybor(self,karta=1000):
         zakres = len(self.karty)
-        #print(zakres)
-        print_delay ("Karty PC: "+ str(len(self.karty)),0.005)#+ str(self.karty)
+        print_delay ("Karty PC: "+ str(len(self.karty)),0.005)
         karta = int(zakres)
 
         x,y = stos_kart.karty[-1].split(' ')
         a,b = [' ',' ']
         while x != a and y != b:  
             karta += -1
-            #print("test karty"+str(karta))
-            #print(karta)
             rzucona_karta=self.karty[karta]
             a,b = rzucona_karta.split(' ')
-            #print(a+b)
             if karta==0:
                 print_delay("PC dobiera kartę")
                 return 0
@@ -95,10 +90,41 @@ class stos: #stos zagranych kart
     def wyswietl(self):
         print(self.karty)
 
+#MECHANIKI MENU
+def menu():
+    print_delay("---------MENU---------\n")
+    print_delay(" *ROZPOCZNIJ GRĘ* (1))\n \n  *ZASADY GRY* (2)\n")
+    nawiguj = 0
+    while nawiguj not in (1,2):
+        nawiguj=int(input("Wpisz cyfrę 1-2: "))
+    return nawiguj
 
+def zasady():
+    print_delay("Za chwilę zagrasz w uproszczoną wersję gry Makao.\nTwoim celem jest pozbycie sie wszystkich kart.\n\
+Karta która rzucisz musi pasować do karty na stole tak aby była to taka sama figura lub taki sam kolor.\n\
+Możesz rzucić tylko jedną kartę.\nPowodzenia.\n\
+\n *ROZPOCZNIJ GRĘ* (1)\n\n\
+     *MENU* (2)\n")
+    nawiguj = 0
+    while nawiguj not in (1,2):
+        nawiguj = int(input("Wpisz cyfrę 1-2: "))
+    return nawiguj
+
+def nawigacja():
+    nawiguj = menu()
+    if nawiguj==2:
+        nawiguj = zasady()
+        if nawiguj==2:
+            nawigacja()
+        elif nawiguj==1:
+            pass
+    elif nawiguj==1:
+        pass
+
+#WYWOŁANIE MENU
+nawigacja()
 #ROZGRYWKA
     #PRYGOTOWANIE TALII
-
 figury=['dwójka','trójka','czwórka','piątka','szóstka','siódemka','ósemka','dziewiątka','dziesiątka','walet','dama','król','as']
 kolory=['pik','trefl','karo','kier']
 talia_bazowa=[]
@@ -106,11 +132,7 @@ for figura in figury:
     for kolor in kolory:
         talia_bazowa.append(figura +' '+ kolor)
 
-talia_uzyta = talia(talia_bazowa)
-
-#nastepna_karta = talia_uzyta.losowanie()
-#print(nastepna_karta)
-#talia_uzyta.wyswietl()   
+talia_uzyta = talia(talia_bazowa) 
  
     #ROZDANIE KART
 gracz1 = reka([])
@@ -122,13 +144,9 @@ for x in range(5):
     
 stos_kart = stos([])
 stos_kart.dodanie(talia_uzyta.losowanie())
-#print ("Karta na stole to: " + stos_kart.karty[-1])
-#gracz1.wyswietl()
-#gracz2.wyswietl()
-#talia_uzyta.wyswietl()
 
     #ROZGRYWKA WŁAŚCIWA
-def rzucanie_karty(rzucona_karta):
+def tura_gracza(rzucona_karta):
     if rzucona_karta!=0:
         x,y = stos_kart.karty[-1].split(' ')
         a,b = [' ',' ']
@@ -136,12 +154,10 @@ def rzucanie_karty(rzucona_karta):
             a,b = rzucona_karta.split(' ')
             if x != a and y != b:
                 print_delay("\nNie możesz rzucić tej karty!")
-                rzucanie_karty(rzucona_karta = gracz1.wybor())
+                tura_gracza(rzucona_karta = gracz1.wybor())
             else:
                 gracz1.usun(rzucona_karta)
-                #gracz1.wyswietl()
                 stos_kart.dodanie(rzucona_karta)
-                #stos_kart.wyswietl()
     else:
         print_delay("dobieraniekarty.exe")
         gracz1.dobierz(talia_uzyta.losowanie())
@@ -153,21 +169,17 @@ def tura_PC(rzucona_karta):
     else:
         gracz2.dobierz(talia_uzyta.losowanie())
 
-
 tura=0
 while len(gracz1.karty) > 0 and len(gracz2.karty) > 0: #WARUNKI ZAKOŃCZENIA GRY
     tura+=1 
-    #print(talia_uzyta.policz())
     if talia_uzyta.policz()==0: #sprawdzanie czy talia jest niepusta 
-        #print (stos_kart.karty)
         talia_uzyta = talia(stos_kart.karty)
         stos_kart=stos([talia_uzyta.karty[-1]])
         talia_uzyta.usun(talia_uzyta.karty[-1])
-        #print (talia_uzyta.karty)
     
     print_delay ('\ngramy dalej')
     print_delay ("Karta na stole to: " + stos_kart.karty[-1])
-    rzucanie_karty(gracz1.wybor())
+    tura_gracza(gracz1.wybor())
     if len(gracz1.karty)==0:
         print_delay ("\n------WYYGRAŁEŚ!------\nSkonczyłeś grę w "+str(tura)+" turach\n------GRATULACJE------",0.1)
         break
